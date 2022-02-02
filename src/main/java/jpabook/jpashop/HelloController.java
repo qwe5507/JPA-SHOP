6eb11c69e3ac918811b171ab8b72924d77f0f8fa
qwe5507 <apvmffkdls@naver.com>
2022-02-03 07:12:03 +0900

    엔티티 클래스 개발

--- src/main/java/jpabook/jpashop/HelloController.java
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class HelloController {
 
-//    @GetMapping("hello")
-//    public String hello(Model model) {
-//        System.out.println("/happy 요청");
-//        model.addAttribute("data", "happy");
-//        return "hello";
-//    }
+    @GetMapping("hello")
+    public String hello(Model model) {
+        System.out.println("/happy 요청");
+        model.addAttribute("data", "happy");
+        return "hello";
+    }
 //    @GetMapping("hello2")
 //    public String hello2() {
 //        System.out.println("hello2 요청");
