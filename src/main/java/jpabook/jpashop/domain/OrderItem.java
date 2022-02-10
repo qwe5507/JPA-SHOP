01164e75423a24ce473d950be7e4128eaf8a09a7
qwe5507 <apvmffkdls@naver.com>
2022-02-10 23:12:44 +0900

    주문 서비스 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,14 +1,21 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
+
+//    protected OrderItem() {
+//    }
+
     @Id @GeneratedValue
     @Column(name = "order_item_id")
     private Long id;
